7a0f3c34ffe00eb40b816f7253eb4bbae5ab5b02
Anastasia <nasty-n@mail.ru>
2020-01-22 11:37:13 +0100

    22.01.2020

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,22 +1,32 @@
 let userCar: Car;
 
+// добавить машину
+
 function createCar() {
   let plate = (<HTMLInputElement>document.getElementById("plate")).value;
-  let color = (<HTMLInputElement>document.getElementById("color")).value;
-  let brand = (<HTMLInputElement>document.getElementById("brand")).value;
+  const expresion = /^[0-9]{4}[a-zA-Z]{3}$/;
+  if (expresion.test(plate) == true) {
+    let color = (<HTMLInputElement>document.getElementById("color")).value;
+    let brand = (<HTMLInputElement>document.getElementById("brand")).value;
 
-  userCar = new Car(plate, color, brand);
+    userCar = new Car(plate, color, brand);
 
-  (<HTMLInputElement>document.getElementById("infoCar")).innerText =
-    "PLATE: " +
-    userCar.plate +
-    " COLOR: " +
-    userCar.color +
-    " BRAND: " +
-    userCar.brand;
+    (<HTMLInputElement>document.getElementById("infoCar")).innerText =
+      "PLATE: " +
+      userCar.plate +
+      " COLOR: " +
+      userCar.color +
+      " BRAND: " +
+      userCar.brand;
 
-  showWheelForm();
+    showWheelForm();
+  } else {
+    alert("La matricula no es correcta. Tiene mantener 4 numeros y 3 letras");
+    return;
+  }
 }
+
+// скрыть/показать анкету
 
 function showWheelForm() {
   const carForm = <HTMLInputElement>document.getElementById("carForm");
@@ -24,6 +34,8 @@ function showWheelForm() {
   carForm.classList.add("d-none");
   carWheel.classList.remove("d-none");
 }
+
+// добавить колеса
 
 function createWheel() {
   for (let i = 1; i < 5; i++) {
@@ -37,33 +49,55 @@ function createWheel() {
   (<HTMLInputElement>document.getElementById("infoWheel")).innerText =
     " WHEELS: " + JSON.stringify(userCar.wheels);
 }
-/* for (i = 1; i < 5; i++) {
-    let diameter = (<HTMLInputElement>document.getElementById("diameter[i]")).valueAsNumber;
-    let brand = (<HTMLInputElement>document.getElementById("brand[i]]")).value;
-    userCar.addWheel(new Wheel(diameter, brand));
+
+/* 
+
+let plateSymbol: any = plate.split("");
+
+_____________ не работает
+ 
+    if (plateSymbol.length < 8) {
+      for (let i = 0; i < plateSymbol.length; i++) {
+      if (!isNaN(plateSymbol.slice(0, 3)) && isNaN(plateSymbol.slice(4, 6))) {
+        break;
+      } else {
+        alert("Matricula tiene que mantener 4 numeros y 3 letras");
+      }
+    } else {
+      alert("Matricula no puede tener mas que 7 simbolos!");
+      break;
+    }
+  }
+experession regular
+
+______________ работает. Нужно ли?
+
+if (plateSymbol.length < 8) {
+      alert("ok");
+      
+    } else {
+      alert("Matricula no puede tener mas que 7 simbolos!");
     }
 
-let diameter = (<HTMLInputElement>document.getElementById("diameter1"))
-    .valueAsNumber;
-  let brand = (<HTMLInputElement>document.getElementById("brand1")).value;
+_______________ работает, но усложнила.
 
-  let wheel = new Wheel(diameter, brand);
-  userCar.addWheel(wheel);
+    function matricula() {
+  let plate = (<HTMLInputElement>document.getElementById("plate")).value;
 
+  let countNum = 0;
+  let countLetra = 0;
 
-на подумать!!!
-for (i=0; i<gearInputs.length; i++) {
-var gearName = gearInputs[i].value,
-  gearButton = document.getElementById(i);
-  if(gearName && gearName != ''){
-    gearButton.innerHTML = '<span class="resultsButtons">' + gearName +  '</span>';
+  let plateSymbols: any = plate.split("");
+
+  for (let plateSymbol of plateSymbols) {
+    if (!isNaN(plateSymbol)) {
+      alert("numero");
+      countNum++;
+    } else {
+      alert("letra");
+      countLetra++;
+    }
   }
+}
 
-
-    parseInt
-
-    для диаметра колес 
-  car.addWheel(new Wheel(diameter, brand));
-  car.addWheel(new Wheel(diameter, brand));
-  (<HTMLInputElement>document.getElementById("infoWheel")).innerText =
-    " WHEELS: " + JSON.stringify(car.wheels);*/
+*/
